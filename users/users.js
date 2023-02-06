be6cec9a9a45f5ae59ef4333ed035be502524e9e
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2023-02-06 23:02:40 +0600

    Промежуточные обработчики

--- users/users.js
+++ users/users.js
@@ -3,6 +3,11 @@ import express
 
 const userRouter = express.Router()
 
+userRouter.use((req, res, next) => {
+  console.log('Обработчик users')
+  next()
+})
+
 userRouter.post('/login', (req, res) => {
   res.send('login')
 })
