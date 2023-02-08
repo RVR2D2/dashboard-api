9d6786b4508a53266347252735948157f4189d3f
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2023-02-08 19:49:28 +0600

    Переход на тс

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-import express
-  from 'express';
-import {
-  userRouter
-} from "./users/users.js";
-
+import express from 'express';
+import { userRouter } from "./users/users";
 const port = 8000;
-const app = express()
-
+const app = express();
 app.use((req, res, next) => {
-  console.log('Время ', Date.now())
-  next()
-})
-
+    console.log('Время ', Date.now());
+    next();
+});
 app.get('/hello', (req, res) => {
-  throw new Error('Error')
-})
-
-app.use('/users', userRouter)
-
-app.use((err,req, res, next) => {
-  console.log(err?.message)
-  res?.status(500).send(err?.message)
-})
-
+    throw new Error('Error');
+});
+app.use('/users', userRouter);
+app.use((err, req, res, next) => {
+    console.log(err === null || err === void 0 ? void 0 : err.message);
+    res === null || res === void 0 ? void 0 : res.status(500).send(err === null || err === void 0 ? void 0 : err.message);
+});
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`)
-})
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
--- users/users.js
+++ users/users.js
@@ -1,21 +1,13 @@
-import express
-  from "express"
-
-const userRouter = express.Router()
-
+import express from "express";
+const userRouter = express.Router();
 userRouter.use((req, res, next) => {
-  console.log('Обработчик users')
-  next()
-})
-
+    console.log('Обработчик users');
+    next();
+});
 userRouter.post('/login', (req, res) => {
-  res.send('login')
-})
-
+    res.send('login');
+});
 userRouter.post('/register', (req, res) => {
-  res.send('register')
-})
-
-export {
-  userRouter
-}
+    res.send('register');
+});
+export { userRouter };
