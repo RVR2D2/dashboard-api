928379c5f4661e9a4b1bd951a3e6d3797f309e08
Vadim Redkin <59052910+RVR2D2@users.noreply.github.com>
2023-02-13 20:46:33 +0600

    Базовый класс контроллера

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import {
-  App
-} from "./app";
-import {
-  LoggerService
-} from "./logger/logger.service";
+import { App } from './app'
+import { LoggerService } from './logger/logger.service'
 
 async function bootstrap() {
-  const app = new App(new LoggerService())
-  await app.init()
+	const app = new App(new LoggerService())
+	await app.init()
 }
 
 bootstrap()
